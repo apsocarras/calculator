0227fadd6103435d8f05ea1c19326b12ef00b902
Alejandro Socarras <apsocarras@gmail.com>
2022-10-17 16:28:44 -0600

    Adding functions 'add()' and 'saySomething'
    Co-authored-by: VW <vera.weikel@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,8 @@ function saySomething(whatToSay) {
 function add(number1, number2) {
   return number1 + number2;
 }
+
+// saySomething("hi");
+// const result = add(3,5);
+// const outputText = "The sum is " + result + "."; 
+// saySomething(outputText);    

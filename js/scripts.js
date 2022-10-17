95408e9dcd0b6515383f95019ecaf567d43f3c49
Alejandro Socarras <apsocarras@gmail.com>
2022-10-17 16:51:54 -0600

    Parsing int from prompt() input
    Co-authored-by: VW <vera.weikel@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,25 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
+// function saySomething(whatToSay) {
+//   window.alert(whatToSay);
+// }
 
+// business logic
 function add(number1, number2) {
   return number1 + number2;
 }
+
+// user interface logic 
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+window.alert(add(number1, number2));
+
+
+
+// const additionResult = add(10, 5);
+// window.alert(additionResult); //implicitly converts to string
 
 // saySomething("hi");
 // const result = add(3,5);
 // const outputText = "The sum is " + result + "."; 
 // saySomething(outputText);    
 
-saySomething("The sum is " + add(3,5) + ".");
+// saySomething("The sum is " + add(3,5) + ".");

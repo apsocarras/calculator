6451ab5f646f73bd87872b67782432cd675ce10c
Alejandro Socarras <apsocarras@gmail.com>
2022-10-17 18:48:53 -0600

    Add eventListener, radio buttons on inputs
    Co-authored-by: VW <vera.weikel@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -22,9 +22,29 @@ function division(divisor1, divisor2){
   return divisor1/divisor2;
 }
 
-// user interface logic 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+// // user interface logic 
+
+function handleCalculation(event){
+  event.preventDefault();
+
+  const number1 = parseInt(document.querySelector("input#inputNumberOne").value);
+  const number2 = parseInt(document.querySelector("input#inputNumberTwo").value);
+
+  const operator = document.querySelector("input[name='operator']:checked").value;
+  console.log("Number 1: " + number1);
+  console.log("Number 2: " + number2);
+  console.log("Operator:", operator);
+
+  
+}
+
+window.addEventListener("load", function(){
+  const form = document.getElementById("calculator");
+  form.addEventListener("submit", handleCalculation);
+});
+
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
 
 const addDisplay = ("This is the addition result: " + number1 + " + " + number2 + " = " + add(number1, number2) + ". " + `\n`); 
 const subDisplay = ("This is the subtraction result: " + number1 + " - " + number2 + " = " + subtraction(number1, number2) + ". " + `\n`);
@@ -32,7 +52,7 @@ const multDisplay = ("This is the multiplication result: " + number1 + " * " + n
 const divDisplay = ("This is the division result: " + number1 + " / " + number2 + " = " + division(number1, number2) + ". " + `\n`);
 
 
-window.alert(addDisplay + subDisplay + multDisplay + divDisplay);
+// window.alert(addDisplay + subDisplay + multDisplay + divDisplay);
 
 
 
